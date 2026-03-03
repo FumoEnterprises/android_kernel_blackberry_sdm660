17c659d43c0b44c1fd3d48d37116b38bd924a2ce
Johnathan Cintron <squash@shadowfall.me>
2026-03-02 20:42:27 -0500

    focaltech: gate virtual key reporting on fts_buttons_enabled state

--- drivers/input/touchscreen/focaltech_bbry/focaltech_report.c
+++ drivers/input/touchscreen/focaltech_bbry/focaltech_report.c
@@ -38,6 +38,7 @@
 #include <linux/kthread.h>
 #include "focaltech_comm.h"
 #include "focaltech_core.h"
+extern bool fts_get_buttons_enabled(void);
 
 /*******************************************************************************
 * Private constant and macro definitions using #define
@@ -220,6 +221,10 @@ void btn_mask_on_notif(bool btn_mask_on_change)
 
 static void fts_nav_key_report_work(struct work_struct *work)
 {
+	if (!fts_get_buttons_enabled()) {
+		nav_key_is_reporting = 0;
+		return;
+	}
 	if (nav_key_need_report) {
 		printk("[haojun] %s report nav key down x = %d, y = %d key_num = %d \n", 
 				__func__, nav_key_position_x, nav_key_position_y, nav_key_position_id);
@@ -238,6 +243,8 @@ static void fts_nav_key_report_work(struct work_struct *work)
 
 static void fts_nav_key_report_down_up(void)
 {
+	if (!fts_get_buttons_enabled())
+		return;
 	printk("[haojun] %s \n", __func__);
 	input_mt_slot(g_input_dev, nav_key_position_id);
 	input_report_abs(g_input_dev, ABS_MT_PRESSURE,0x3f/*data->pressure[i]*/);
@@ -740,6 +747,10 @@ static int fts_input_dev_report_B(struct ts_event *data)
 
 	for (i = 0; i < data->touch_point; i++)
 	{
+		 /* Skip touches in the virtual key zone when fts_buttons_enabled is disabled */
+		 if (!fts_get_buttons_enabled() && FTS_KEY_ALL_Y_POS == data->au16_y[i])
+			continue;
+
 		 input_mt_slot(g_input_dev, data->au8_finger_id[i]);
 
 		if ((data->au8_touch_event[i]== DOWN_EVENT)
@@ -841,6 +852,10 @@ static int fts_input_dev_report_key_event(struct ts_event *data)
  {
 	int i = 0;
 	int bUsedKeyEvent = 1;//0:not used, 1: used
+
+	/* gate on fts_buttons_enabled sysfs toggle */
+	if (!fts_get_buttons_enabled())
+		return -1;
 
 #if !defined(CONFIG_TCT_SDM660_COMMON)
 	/*is enable?*/
